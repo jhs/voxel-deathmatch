fb04099011128d8229377ada6f8f5ac1ffc4fdf6
Jason Smith (work) <jason.h.smith@gmail.com>
2014-09-11 09:06:23 +0000

    First crack, not really working

--- game.js
+++ game.js
@@ -1,4 +1,26 @@
-// Beefy test
+var createGame = require('voxel-engine')
 
-var url = require('url')
-document.body.innerHTML = JSON.stringify(url.parse(window.location.href))
+var container = document.querySelector('#game')
+var game = createGame({ startingPosition: [0, 1000, 0]
+                      //, texturePath: texturePath
+                      , materials: [['grass', 'dirt', 'grass_dirt'], 'bedrock']
+                      , generate: generate_world
+                      })
+
+window.game = game // for debugging
+//game.controls.pitchObject.rotation.x = -1.5 // Look down.
+game.appendTo(container)
+game.currentMaterial = 1
+
+if(0)
+container.addEventListener('click', function() { 
+  game.requestPointerLock(container)
+})
+
+function generate_world(x, y, z) {
+  if (y == 0 && x > -4 && x < 4 && z > -4 && z < 4)
+    return 1
+  if (y == -20 && x > -20 && x < 20 && z > -20 && z < 20)
+    return 2
+  return 0
+}
